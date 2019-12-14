a9421b3882a9aaef1b2af0adb99bc797828d262f
Vishal Lakshmi <vishal.rox26@gmail.com>
2019-12-14 15:59:01 -0800

    Updates

---
+++ precache-manifest.b8ee35c28ae41ae47d2d3a9538836819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb61e154b8faa7ba8c17bc20034e4853",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c5f49924345d170f85d8",
+    "url": "/monster-rolodex/static/css/main.eae23c8b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5f49924345d170f85d8",
+    "url": "/monster-rolodex/static/js/main.c7c7c741.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
